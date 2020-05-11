61729fd9edac1a8f9cde7931225575826de4d934
Rinda Nur Hafizha <rindanurh@gmail.com>
2020-05-11 23:15:50 +0700

    test log

--- src/components/App.js
+++ src/components/App.js
@@ -414,6 +414,8 @@ class App extends React.Component {
 
 
 	render() {
+
+		console.log("hi aku ngetest ya sayank")
 		const latestDate = this.state.latestDate;
 		const first = this.state.firstDate
 
